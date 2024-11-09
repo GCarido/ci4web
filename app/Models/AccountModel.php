ab971ac0cab7277d0b860b8a9a159cb77cd9845a
Gerald Carido <112929169+GCarido@users.noreply.github.com>
2024-11-09 19:30:01 +0800

    Add account model

---
+++ app/Models/AccountModel.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Models;
+use CodeIgniter\Model;
+
+class AccountModel extends Model
+{
+    protected $table = 'accounts';
+    protected $primaryKey = 'account_id';
+    protected $allowedFields = [
+        'name',
+        'email',
+        'password',
+        'token',
+        'type'
+    ];
+}
