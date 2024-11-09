090d2dee95f964492623d7799ca2820a254331f1
Gerald Carido <112929169+GCarido@users.noreply.github.com>
2024-11-09 14:46:21 +0800

    Add mail method to controller

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -80,4 +80,30 @@ class Home extends BaseController
 
         return redirect()->to('/product'); // Redirect
     }
+
+    public function sendMail()
+    {
+        $to = 'erningcards@gmail.com';
+        $subject = 'Test Email PHP';
+        $message = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eum, accusamus temporibus illum vel sint non. Mollitia tempore dignissimos ut perferendis sint? Autem minus culpa assumenda ipsa quos veniam distinctio facilis!';
+        $email = \Config\Services::email();
+        $email->setTo($to);
+        $email->setFrom(
+            'geraldcastcarido@gmail.com',
+            $subject
+        );
+        $email->setMessage($message);
+
+        if($email->send()){
+            echo "Email sent successfully";
+        } else{
+            $data = $email->printDebugger(['headers']);
+            print($data);
+        }
+    }
+
+    public function mail()
+    {
+        $this->sendMail();
+    }
 }
