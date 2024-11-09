1dea3096fe98823a1c5dd4693e1107b8cd082fb0
Gerald Carido <112929169+GCarido@users.noreply.github.com>
2024-11-09 19:29:31 +0800

    Add register method with partial empty data passed

--- app/Controllers/AccountController.php
+++ app/Controllers/AccountController.php
@@ -2,8 +2,19 @@
 
 namespace App\Controllers;
 
+use App\Models\AccountModel;
+
 class AccountController extends BaseController
 {
+
+    public function register()
+    {
+        helper(['form']);
+        $data = [];
+        return view('register', $data);
+    }
+
+
     public function sendMail()
     {
         $to = 'erningcards@gmail.com';
@@ -30,6 +41,35 @@ class AccountController extends BaseController
         $str_result = '0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
 
         return substr(str_shuffle($str_result), 0, $length);
+    }
+
+    public function store()
+    {
+        helper(['form']);
+        $rules = [
+            'name'  => 'required|min_length[5]|max_length[50]',
+            'email' => 'required|min_length[20]|max_length[100]|valid_email|is_unique[accounts.email]',
+            'password' => 'required|min_length[8]|max_length[50]',
+            'confirmpassword' => 'matches[password]'
+        ];
+
+        if($this->validate($rules)){
+            $accountModel = new AccountModel();
+            $data = [
+                'name' => $this->request->getVar('name'),
+                'email' => $this->request->getVar('email'),
+                'password' => password_hash($this->request->getVar('password'), PASSWORD_DEFAULT),
+                'token' => $this->token(100),
+                'type' => 'client'
+            ];
+
+            $accountModel->save($data);
+
+            return redirect('/signin');
+        } else{
+            $data['validation'] = $this->validator;
+            echo view('register', $data);
+        }
     }
 
 }
