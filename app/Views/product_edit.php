97520eb0f0c1f9c61e0530fcd68ace647030d8ad
Gerald Carido <112929169+GCarido@users.noreply.github.com>
2024-11-09 12:04:18 +0800

    Product content initial setup

--- app/Views/product_edit.php
+++ app/Views/product_edit.php
@@ -6,15 +6,12 @@
 </head>
 <body>
     <form action="<?=base_url('/update')?>" method="POST">
-        <?= $storeId = $products['product_id']; ?>
-
-        <? echo $storeId; ?>
+        <input type="hidden" name="product_id" value="<?= $products['product_id']?>">
         <label for="">Name: </label>
         <input type="text" name="name" value="<?= $products['name']?>">
-
         <br>
         <label for="">Description: </label>
-        <input type="text" name="descriptiopn" value="<?= $products['description']?>">
+        <input type="text" name="description" value="<?= $products['description']?>">
         <br>
         <label for="">Quantity </label>
         <input type="text" name="quantity" value="<?= $products['quantity']?>">
@@ -24,8 +21,8 @@
         <br>
         <label for="">Status: </label>
         <select name="status" id="">
-            <option value="active">Active</option>
-            <option value="inactive">Inactive</option>
+            <option value="Active">Active</option>
+            <option value="Inactive">Inactive</option>
         </select>
         <input type="submit" name="active" value="Submit">
     </form>
