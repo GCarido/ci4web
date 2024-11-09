478541538bf04b41871af4001f3edca8b08a85f0
Gerald Carido <112929169+GCarido@users.noreply.github.com>
2024-11-09 14:04:18 +0800

    Add jquery

--- app/Views/products.php
+++ app/Views/products.php
@@ -5,13 +5,14 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous" defer></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <title>Document</title>
 </head>
 
 <body>
     <?php if (session()->getFlashdata('msg')): ?>
         <div class="alert alert-success">
-            <?= session()->getFlashdata('msg')?>
+            <?= session()->getFlashdata('msg') ?>
         </div>
     <?php endif; ?>
     <table border="1">
@@ -38,6 +39,48 @@
             </tr>
         <?php endforeach; ?>
     </table>
+
+    <!-- Button trigger modal -->
+
+    <?php foreach ($products as $item): ?>
+        <?php extract($item) ?>
+        <button type="button" class="btn btn-primary md" data-bs-toggle="modal" data-bs-target="#exampleModal" data-price="<?= $price ?>" data-description="<?= $description ?>">
+            <?= $name ?>
+        </button>
+    <?php endforeach; ?>
+
+
+    <!-- Modal -->
+    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body">
+                    <p class="test1"></p>
+                    <input type="text" class="test2">
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
+
+<script>
+    $(document).ready(function(){
+        $(".md").click(function(){
+            let price = $(this).attr("data-price");
+            let description = $(this).attr("data-description");
+
+            $(".test1").text(price);
+            $(".test2").val(description);
+        });
+    });
+</script>
 
 </html>
